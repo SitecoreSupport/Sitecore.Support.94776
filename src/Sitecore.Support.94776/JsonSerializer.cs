b11dff29b970e5a44079431c45aafbd5291c7f68
Vladyslav Molibozhenko <vlm@sitecore.net>
2017-03-22 18:42:52 +0200

    Initial commit

---
+++ src/Sitecore.Support.94776/JsonSerializer.cs
@@ -0,0 +1,31 @@
+﻿namespace Sitecore.Support.ItemWebApi.Serialization
+{
+    using Sitecore.Configuration;
+    using Sitecore.Diagnostics;
+    using Sitecore.ItemWebApi.Serialization;
+    using System;
+    using System.Globalization;
+    using System.Web.Script.Serialization;
+
+    public class JsonSerializer : ISerializer
+    {
+        public string Serialize(object value)
+        {
+            int num;
+            Assert.ArgumentNotNull(value, "value");
+            JavaScriptSerializer serializer = new JavaScriptSerializer
+            {
+                MaxJsonLength = 0x200000
+            };
+            string setting = Settings.GetSetting("JsonSerialization.MaxLength");
+            if ((!string.IsNullOrEmpty(setting) && !serializer.MaxJsonLength.ToString(CultureInfo.InvariantCulture).Equals(setting, StringComparison.InvariantCultureIgnoreCase)) && int.TryParse(setting, out num))
+            {
+                serializer.MaxJsonLength = num;
+            }
+            return serializer.Serialize(value);
+        }
+
+        public string SerializedDataMediaType =>
+            "application/json";
+    }
+}
---
+++ src/Sitecore.Support.94776/RewriteUrl.cs
@@ -0,0 +1,66 @@
+﻿using Sitecore.Diagnostics;
+using Sitecore.ItemWebApi;
+using Sitecore.Support.ItemWebApi.Serialization;
+using Sitecore.ItemWebApi.Web;
+using Sitecore.Pipelines.PreprocessRequest;
+using Sitecore.Text;
+using Sitecore.Web;
+using System;
+using System.Web;
+
+namespace Sitecore.Support.ItemWebApi.Pipelines.PreprocessRequest
+{
+    public class RewriteUrl : Sitecore.ItemWebApi.Pipelines.PreprocessRequest.RewriteUrl
+    {
+        private static int GetVersion(string path)
+        {
+            int num;
+            Assert.ArgumentNotNull(path, "path");
+            string str = path.TrimStart(new char[] { '/' }).Split(new char[] { '/' })[2];
+            Assert.IsTrue(str.StartsWith("v"), "Version token is wrong.");
+            Assert.IsTrue(int.TryParse(str.Replace("v", string.Empty), out num), "Version not recognized.");
+            return num;
+        }
+
+        public override void Process(PreprocessRequestArgs arguments)
+        {
+            Assert.ArgumentNotNull(arguments, "arguments");
+            try
+            {
+                string localPath = arguments.Context.Request.Url.LocalPath;
+                if (localPath.StartsWith("/-/item/"))
+                {
+                    Sitecore.ItemWebApi.Context context = new Sitecore.ItemWebApi.Context
+                    {
+                        Serializer = new JsonSerializer(),
+                        Version = GetVersion(localPath),
+                        ResponseOutputBuilder = new ResponseOutputBuilder()
+                    };
+                    Sitecore.ItemWebApi.Context.Current = context;
+                    Rewrite(arguments.Context);
+                }
+            }
+            catch (Exception exception)
+            {
+                Logger.Error(exception);
+            }
+        }
+
+        private static void Rewrite(HttpContext context)
+        {
+            Assert.ArgumentNotNull(context, "context");
+            Uri url = context.Request.Url;
+            string[] sourceArray = url.LocalPath.TrimStart(new char[] { '/' }).Split(new char[] { '/' });
+            int length = sourceArray.Length - 3;
+            string[] destinationArray = new string[length];
+            Array.Copy(sourceArray, 3, destinationArray, 0, length);
+            string str2 = $"/{string.Join("/", destinationArray)}";
+            string str3 = url.Query.TrimStart(new char[] { '?' });
+            WebUtil.RewriteUrl(new UrlString
+            {
+                Path = str2,
+                Query = str3
+            }.ToString());
+        }
+    }
+}
