a173be2b772559860060bfc0078b752f4665e0e8
Andriy Kravets <andriy.kravets@sitecore.com>
2020-04-06 15:33:52 +0300

    Fix commit

---
+++ src/Sitecore.Support.94776/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.94776")]
+[assembly: AssemblyProduct("Sitecore.Support.94776")]
+[assembly: ComVisible(false)]
